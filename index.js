525aa878e3b97ca8aafc41816cfa52468a865851
jstray <jonathanstray@gmail.com>
2017-05-07 16:47:34 -0400

    Now scrolling the zoom

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var SerialPort = require('serialport');
 var port = new SerialPort('/dev/cu.usbmodem1421', {
   baudRate: 115200
@@ -6,3 +7,30 @@ var port = new SerialPort('/dev/cu.usbmodem1421', {
 port.on('data', function (data) {
   console.log(data.toString('utf-8'));
 });
+*/
+
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var browser = new webdriver.Builder()
+  .forBrowser('chrome')
+  .build();
+ 
+var cel; // canvas element
+
+var i=0;
+
+function loop() {
+	i += 1;
+	browser.actions().mouseMove(cel, {x:i*10, y:i*5}).perform();
+}
+
+browser.get('http://www.gigapan.com/gigapans/117375');
+browser.findElement(webdriver.By.tagName("canvas"))
+	.then( el => {
+		cel = el;
+		browser.actions().click(cel).click(cel).mouseMove(cel, {x:0, y:0}).mouseDown(cel).perform();
+
+		//cel.getLocation().then( loc => console.log(loc) )
+		setInterval(loop, 100);
+	});
+
